eb2f68a86471230189472522e4e92f7abb55ff27
c-bringer <corentin.bringer@limayrac.fr>
2019-10-01 16:17:00 +0200

    prepa auth

---
+++ DAO/DAO.php
@@ -0,0 +1,25 @@
+<?php
+class DAO {
+    protected $pdo = NULL; //Objet de connexion
+
+    function __construct() {
+        //Paramètres à partir du init.php
+        $user = DB_USER;
+        $password = DB_PASSWORD;
+        $host = DB_HOST;
+        $name = DB_NAME;
+
+        //On construit le DSN
+        $dsn = 'mysql:host='.$host.';dbname='.$name;
+
+        //Création de la connexion
+        try {
+            $pdo = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo("<p>Erreur lors de la connexion : " . $e->getMessage().'<p>');
+        }
+
+        $this->pdo = $pdo;
+    }
+}
---
+++ include/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+    <div class="container"><a class="navbar-brand" href="#">FREDI</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item" role="presentation"><a class="nav-link <?php if(isset($_SESSION['user'])) { echo "disaled"; } ?>" href="#">Période</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link <?php if(isset($_SESSION['user'])) { echo "disaled"; } ?>" href="#">Note de frais</a></li>
+            </ul>
+            <span class="navbar-text actions">
+                <?php
+                if(isset($_SESSION['user'])) {
+                    echo '
+                    <a class="btn btn-light action-button" role="button" href="mon_compte.php">Mon compte</a>
+                    ';
+                } else {
+                    echo '
+                    <a class="login" href="connexion.php">Connexion</a>
+                    <a class="btn btn-light action-button" role="button" href="inscription.php">Inscription</a>
+                    ';
+                }
+                ?>
+            </span>
+        </div>
+    </div>
+</nav>
