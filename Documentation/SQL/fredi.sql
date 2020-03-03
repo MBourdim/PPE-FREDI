58d91286050ae8445d6aa193903be5ce63f78a65
c-bringer <corentin.bringer@limayrac.fr>
2020-03-03 14:16:47 +0100

    sql

--- Documentation/SQL/fredi.sql
+++ Documentation/SQL/fredi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 03 mars 2020 à 13:10
+-- Généré le :  mar. 03 mars 2020 à 14:16
 -- Version du serveur :  10.4.8-MariaDB
 -- Version de PHP :  7.3.11
 
@@ -167,8 +167,6 @@ CREATE TABLE `ligne_de_frais` (
 --
 
 INSERT INTO `ligne_de_frais` (`id_ligne`, `date_frais`, `lib_trajet`, `cout_peage`, `cout_repas`, `cout_hebergement`, `nb_km`, `total_km`, `total_ligne`, `code_statut`, `id_motif`, `annee`, `id_note`) VALUES
-(2, '2019-12-17', 'Toulouse - Mazamet', '35', '35', '35', 100, '2100', '0', 3, 3, 2019, 1),
-(3, '2019-12-17', 'Mazamet - Toulouse', '10', '10', '10', 100, '2100', '2130', 0, 2, 2019, 2),
 (10, '2019-12-17', 'Mazamet - Toulouse', '10', '10', '0', 100, '2100', '2120', 0, 0, 0, 0),
 (11, '2019-12-17', 'Mazamet - Toulouse', '10', '10', '0', 100, '2100', '2120', 0, 0, 2019, 0),
 (12, '2019-12-27', 'Mazamet', '343', '353557', '0', 100, '2100', '356000', 0, 2, 2019, 0),
@@ -349,9 +347,49 @@ CREATE TABLE `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `nom`, `prenom`, `email`, `password`, `code_statut`, `id_type_utilisateur`) VALUES
-(8, 'Controlleur', 'Compte', 'compte@controlleur.fr', '$2y$10$4bV1UXHFo.Cisy5aNfHIC.xutl7mo2ty.jd1J5LAMNxobpPX7JvPG', 1, 2),
-(9, 'Administrateur', 'Compte', 'compte@administrateur.fr', '$2y$10$zU7N3JVr9vknvaQIEz2NuOizVH6Fu5XomQ8unmbDlSyGG6zcDHcPm', 1, 1),
-(10, 'Adherent', 'Compte', 'compte@adherent.fr', '$2y$10$4WhQCKmDVre9BtQ1B8.47ejYAVJ3VKMcmAV.Im4aasQ7O/BCMEZ3y', 1, 3);
+(1, 'BANDILELLA', 'CLEMENT', 'BANDILELLA@BANDILELLA.fr', '$2y$10$MhqPrY1I.kGoIB3cVzBQl.Ccob6m8QgYRTeVCpVADm/iDUuFE1Xxm', 1, 1),
+(2, 'BERBIER', 'LUCILLE', 'BERBIER@BERBIER.fr', '$2y$10$XIXS44ESJAFwhjUtN0/J9eeapdTLHWbBi.xP4tpw/D38Waakw9Qwu', 1, 1),
+(3, 'BERBIER', 'THEO', 'BERBIER@BERBIER.fr', '$2y$10$Ab23t3qHgIQLbLtf9ASWkO.fn9uDgo8FVyW8zgLnnij1xa36d/Td6', 1, 1),
+(4, 'BECKER', 'ROMAIN', 'BECKER@BECKER.fr', '$2y$10$jlXf9eSyv8VAOL2lpN494uLmYW2qSm56d5rL.R/gM4zRQVsqOXbIq', 1, 1),
+(5, 'BIACQUEL', 'VERONIQUE', 'BIACQUEL@BIACQUEL.fr', '$2y$10$Phk2GWJ6HNjTMsn.r2v.z.GZi6J5AajMiNuMBICfQ0ujm0ihM1IAq', 1, 1),
+(6, 'BIDELOT', 'BRIGITTE', 'BIDELOT@BIDELOT.fr', '$2y$10$Kp1256r0S3l5EWkrk1Iq5e4DfAcObER9ZD8VEldj3sBGEoTCDi2l2', 1, 1),
+(7, 'BIDELOT', 'JULIE', 'BIDELOT@BIDELOT.fr', '$2y$10$aRY9z9P8WWXKAaPGPxUwb.0mlSBo0Hay3mPWWpreem1I7uDfBM0vm', 1, 1),
+(8, 'BILLOT', 'DIDIER', 'BILLOT@BILLOT.fr', '$2y$10$1DXcnVY9f53Nh91eARenEu7SoDp6T5zodzgoofq4cZg3a4PHQQymW', 1, 1),
+(9, 'BILLOT', 'CLAIRE', 'BILLOT@BILLOT.fr', '$2y$10$gZzGd2sAqTpu54PdQfrEIub7IrTefqmfpgoU32CVVgqHbjyYMWDna', 1, 1),
+(10, 'BILLOT', 'MARIANNE', 'BILLOT@BILLOT.fr', '$2y$10$tQbRkYhbr8ScZx.lqkRP3OB3.9H6HQbHZHUgAxjrK.XHa2fRqvmTe', 1, 1),
+(11, 'BINNET', 'MARIUS', 'BINNET@BINNET.fr', '$2y$10$0.PhF.BHOOQg.6yZtVDMR.Uv05n9fg/Se/HxNq47J.AisRkU1Gx9C', 1, 1),
+(12, 'CALDI', 'THOMAS', 'CALDI@CALDI.fr', '$2y$10$JK7rh8f/hkTIfaI12zqbdOKRlyhIc0kBWN3405Dq9IO3SN4uWC7Fu', 1, 1),
+(13, 'CASTEL', 'TIMOTHE', 'CASTEL@CASTEL.fr', '$2y$10$uhw3mmKWvH9J50AKXzuq0eAMbz/s/reCIZ7TwmgOKHpO.gEWS6ptW', 1, 1),
+(14, 'CHEOLLE', 'NICOLAS', 'CHEOLLE@CHEOLLE.fr', '$2y$10$QPCgSRA7nSVebTIsT4tbMOeMcNzonUvxPgufdyHSUyW900fGxtgyu', 1, 1),
+(15, 'CHERPION', 'UGO', 'CHERPION@CHERPION.fr', '$2y$10$YPBShiLO8wJ.u8A1.Qy4iOvUNkqHHM7NFo6WvwQC3y2yCV37RDS3i', 1, 1),
+(16, 'CHEVOITINE', 'LOUIS', 'CHEVOITINE@CHEVOITINE.fr', '$2y$10$DaPa/wJ1jQgzwSgef.K8q.ERFRvMiH2kvDGn5rKenLu.IMLZCifhe', 1, 1),
+(17, 'CHOUARNO', 'TOM', 'CHOUARNO@CHOUARNO.fr', '$2y$10$7HjDHMQhaT7fcmPCRea1xuDFoV74o5OFCVNJ2PgeRUHri4mReRQou', 1, 1),
+(18, 'COTIN', 'FLORIAN', 'COTIN@COTIN.fr', '$2y$10$2ht3iEOqWSw4CqLo1Yx0.O324bu7o/a0W5zbeO1ufhUMRmcCVN/aS', 1, 1),
+(19, 'DEPERRIN', 'ARNAUD', 'DEPERRIN@DEPERRIN.fr', '$2y$10$lb8Zp8z2ZoF3xJ4VRToEy.THU4LZhN/YiEnWgs1ZoRouuCOACWF/a', 1, 1),
+(20, 'DEPRETRE', 'BEATRICE', 'DEPRETRE@DEPRETRE.fr', '$2y$10$b.aCImehjcfsZrYNDvRH/eekoKBv.Xld6Vp5QHK8C1fua95sR7xha', 1, 1),
+(21, 'DUCRICK', 'AUGUSTIN', 'DUCRICK@DUCRICK.fr', '$2y$10$DePvNOwGDCvXqRY1VmOnfON2lMCj3j6XvtNvQ6FDp.iXr/aXcMjRi', 1, 1),
+(22, 'GARBILLON', 'GILLES', 'GARBILLON@GARBILLON.fr', '$2y$10$syRLqDeaaiwMmRla8fY75eXOvBD1uph2d7HP1sGQpHVsSXiipOMZy', 1, 1),
+(23, 'GARBILLON', 'YANN', 'GARBILLON@GARBILLON.fr', '$2y$10$PpYVLFUijHhL0IYR357tNeAs1Iwklpl7yHF4KZckZVyHMP8AetjNq', 1, 1),
+(24, 'HAGENBACH', 'CLEMENTINE', 'HAGENBACH@HAGENBACH.fr', '$2y$10$nBi2X0VyYSd3fNnbm0aUk.PJDAfIqUNPLUhV9/dOlY.V2ObGSYgtu', 1, 1),
+(25, 'HASFELD', 'AUXANE', 'HASFELD@HASFELD.fr', '$2y$10$5JS9U58N/YWM7doyrjVtLeqyVZWJ/7mZSgouRJ8WKYsqFmnsY2o8y', 1, 1),
+(26, 'HUMERT', 'ISABELLE', 'HUMERT@HUMERT.fr', '$2y$10$gMvG1EAzeN9MiXqtmXzpqexx1X52vXyCddhoviOH8jH9DaMxd2AjK', 1, 1),
+(27, 'LAFIEGLON', 'CLEMENT', 'LAFIEGLON@LAFIEGLON.fr', '$2y$10$MRQy01837N8IQfeLOhKgZ.GRX0KZyrz47PTJQZ/UA5POR0VZfmHNC', 1, 1),
+(28, 'LAMOINE', 'GREGOIRE', 'LAMOINE@LAMOINE.fr', '$2y$10$khMnpodWvs/S/YuD4yVUN.Y/f8s9W2xAJkHgVt74TjjOM8X6drkjK', 1, 1),
+(29, 'LANIELLE', 'NICOLAS', 'LANIELLE@LANIELLE.fr', '$2y$10$3sJWVho4vCf2fknIT6eO6.CyBgcjvlDOE/eIosw1ILHtaEIxvkb9K', 1, 1),
+(30, 'LIEVIN', 'NATHAN', 'LIEVIN@LIEVIN.fr', '$2y$10$n7YFh3GVzmcIy4iNBVaYtOO66Y/iJ/snfL6GyFDWhn68vVaW5nn1G', 1, 1),
+(31, 'LOTANG', 'CYPRIEN', 'LOTANG@LOTANG.fr', '$2y$10$wlIzVZRaK2dz8evEkrC/g.yMQz8g09TGzXhg2JxCfctIZ2HGpeEJG', 1, 1),
+(32, 'LUQUE', 'ETIENNE', 'LUQUE@LUQUE.fr', '$2y$10$..BI3Y6HS2OPi9HjD80gk.knGWdH146Z4DG5lvzZa0sOW.GBSTP1K', 1, 1),
+(33, 'PERNOT', 'PAUL', 'PERNOT@PERNOT.fr', '$2y$10$sHYaeAknklwbEMTfhX2MNOLoMoWQPXvENlyEgygx1F1Vr.dl80KCS', 1, 1),
+(34, 'REMILLON', 'ELIOT', 'REMILLON@REMILLON.fr', '$2y$10$BpVXJTBisU083Jl2QIfHr.0ee6s.V04ZetjK6dGM.RHdbANLXitBC', 1, 1),
+(35, 'SILBERT', 'GILLES', 'SILBERT@SILBERT.fr', '$2y$10$wQridJ9LdN6HKxuZVKsZyO6lozN2dQ/tOvN98QgETPkYBUAKSQF/q', 1, 1),
+(36, 'SILBERT', 'LEA', 'SILBERT@SILBERT.fr', '$2y$10$wWmhW6l5tEn2VUb.KBkHCe4qSwsJ2.1J34d4/WWOcEVnxoE7koo5G', 1, 1),
+(37, 'TORTEMANN', 'PIERRE', 'TORTEMANN@TORTEMANN.fr', '$2y$10$kojmGlanqDBZGxsF5/qm8e7ZkjB85eJCQSN.2ns7eZiZ18pMKVGOC', 1, 1),
+(38, 'ZOECKEL', 'MATHIEU', 'ZOECKEL@ZOECKEL.fr', '$2y$10$MzQ.RFk9JSxVX10CfI995eaMI50.4h.RpzvW1dBiOdC63U3M4WT5K', 1, 1),
+(39, 'ZUEL', 'STEPHANIE', 'ZUEL@ZUEL.fr', '$2y$10$3.fwHyNLAdC4l5J4NhuMAuD33QUsL4hv81jnDgfD7uB7PYhIJBBYm', 1, 1),
+(40, 'ZUERO', 'THOMAS', 'ZUERO@ZUERO.fr', '$2y$10$Ct/CLAKYEGyLZm5SIJnkIeqeXx4rtH8o/UvDklcnv7kbowrr5fjwK', 1, 1),
+(100, 'Controlleur', 'Compte', 'compte@controlleur.fr', '$2y$10$4bV1UXHFo.Cisy5aNfHIC.xutl7mo2ty.jd1J5LAMNxobpPX7JvPG', 1, 2),
+(101, 'Administrateur', 'Compte', 'compte@administrateur.fr', '$2y$10$zU7N3JVr9vknvaQIEz2NuOizVH6Fu5XomQ8unmbDlSyGG6zcDHcPm', 1, 1),
+(102, 'Adherent', 'Compte', 'compte@adherent.fr', '$2y$10$4WhQCKmDVre9BtQ1B8.47ejYAVJ3VKMcmAV.Im4aasQ7O/BCMEZ3y', 1, 3);
 
 --
 -- Index pour les tables déchargées
@@ -472,7 +510,7 @@ ALTER TABLE `type_util`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- Contraintes pour les tables déchargées
